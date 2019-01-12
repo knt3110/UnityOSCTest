7ff4163d9a199f9b9f597edeabff3867abac06da
Kenta Saito <xpenkenta194@gmail.com>
2019-01-12 22:50:40 +0900

    update: 細かい部分の修正

--- OSCTestClient/Assets/Scripts/GameController.cs
+++ OSCTestClient/Assets/Scripts/GameController.cs
@@ -9,6 +9,7 @@ public class GameController : MonoBehaviour {
     public InputField portField;
     public Button startButton;
     public GameObject client;
+    public GameObject canvas2;
 
     public string ip;
     public int port;
@@ -19,12 +20,14 @@ public class GameController : MonoBehaviour {
         startButton.onClick.AddListener(ConnectToServer);
 	}
 
+    // IPアドレスとポート番号を入力してサーバと接続する関数
     public void ConnectToServer()
     {
         ip = ipField.text;
         port = int.Parse(portField.text);
         flag = true;
         GameObject.Find("Canvas").SetActive(false);
+        canvas2.SetActive(true);
         client.SetActive(true);
     }
 }
